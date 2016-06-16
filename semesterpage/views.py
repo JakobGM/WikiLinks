418eb4e1f9f99d1d314d048ba295da87d81fea85
Jakob Gerhard Martinussen <jakobgm@gmail.com>
2016-06-16 12:10:48 +0200

    Set up variables for template rendering of semesterpage

--- semesterpage/views.py
+++ semesterpage/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from .models import StudyProgram, Semester, Course
 
-# Create your views here.
+
 def semester(request, program_code, semester_number):
-    return render(request, 'helloWorld.html')
+    study_program = StudyProgram.objects.get(program_code__iexact=program_code)
+    semester = study_program.semesters.get(number=semester_number)
+    courses = semester.courses.all()
+    return render(request, 'semesterpage/courses.html',
+                  {'semester_number': semester.number, 'courses': courses})

288a2319e74b47ae398958f62bf8d6e43ef383c0
Jakob Gerhard Martinussen <jakobgm@gmail.com>
2016-06-17 18:21:51 +0200

    Add all necessary templates for the semesterpage app

--- semesterpage/views.py
+++ semesterpage/views.py
@@ -4,9 +4,11 @@ from .models import StudyProgram, Semester, Course
 
 def semester(request, program_code, semester_number):
     study_program = StudyProgram.objects.get(program_code__iexact=program_code)
-    semester = study_program.semesters.get(number=semester_number)
+    all_semesters = semester = study_program.semesters.all()
+    semester = all_semesters.get(number=semester_number)
     courses = semester.courses.all()
 
     return render(request, 'semesterpage/courses.html',
                   {'semester_number': semester.number,
-                   'courses': courses})
+                   'courses': courses,
+                   'semesters': all_semesters})

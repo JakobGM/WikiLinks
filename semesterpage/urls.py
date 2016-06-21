f07f69fc9c41ccdba296c7617398bdb74d87d615
Jakob Gerhard Martinussen <jakobgm@gmail.com>
2016-06-21 14:16:21 +0200

    Add semesterpage.urls related to last commit

---
+++ semesterpage/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls import url
+from . import views
+urlpatterns = [
+    url(r'^$', views.semester),
+    url(r'^semester/(?P<semester_number>[1-9]|10)/$', views.semester),
+    url(r'^semester/(?P<semester_number>[1-9]|10)/request$', views.user_request),
+]

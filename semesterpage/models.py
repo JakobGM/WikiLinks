762db761169f87950c52d1afcd99c7ba506595f4
Jakob Gerhard Martinussen <jakobgm@gmail.com>
2016-06-21 15:33:50 +0200

    Add i18n internationalization for norwegian phrases in the admin panel

--- semesterpage/models.py
+++ semesterpage/models.py
@@ -1,22 +1,24 @@
 from django.db import models
 
+from gettext import gettext as _
+
 
 class StudyProgram(models.Model):
     """
     Contains top level information for a specific study program.
     """
-    full_name = models.CharField('fullt navn',
+    full_name = models.CharField(_('fullt navn'),
                                  max_length=60,
-                                 help_text='F.eks. \"Fysikk og matematikk\"'
+                                 help_text=_('F.eks. \"Fysikk og matematikk\"')
                                  )
-    display_name = models.CharField('visningsnavn / kallenavn',
+    display_name = models.CharField(_('visningsnavn / kallenavn'),
                                     max_length=60,
-                                    help_text='F.eks. \"Fysmat\"'
+                                    help_text=_('F.eks. \"Fysmat\"')
                                     )
-    program_code = models.CharField('programkode',
+    program_code = models.CharField(_('programkode'),
                                     primary_key=True,
                                     max_length=10,
-                                    help_text='F.eks. \"MTFYMA\"'
+                                    help_text=_('F.eks. \"MTFYMA\"')
                                     )  # TODO: Only upper case?
 
     def __str__(self):
@@ -31,15 +33,15 @@ class MainProfile(models.Model):
     Contains top level information about a main profile within a given study
     program.
     """
-    full_name = models.CharField('fullt navn',
+    full_name = models.CharField(_('fullt navn'),
                                  max_length=60,
-                                 default='felles',
-                                 help_text='F.eks. \"Industriell matematikk\"'
+                                 default=_('felles'),
+                                 help_text=_('F.eks. \"Industriell matematikk\"')
                                  )
-    display_name = models.CharField('visningsnavn / kallenavn',
+    display_name = models.CharField(_('visningsnavn / kallenavn'),
                                     max_length=60,
-                                    default='Felles',
-                                    help_text='F.eks. \"InMat\"'
+                                    default=_('Felles'),
+                                    help_text=_('F.eks. \"InMat\"')
                                     )
     study_program = models.ForeignKey(StudyProgram,
                                       related_name='mainProfiles'
@@ -57,7 +59,7 @@ class Semester(models.Model):
     """
     Contains an instance of a specific semester connected to a main profile.
     """
-    number = models.PositiveSmallIntegerField('semester (nummer)',
+    number = models.PositiveSmallIntegerField(_('semester (nummer)'),
                                               help_text='F.eks. \"2\"'
                                               )
     study_program = models.ForeignKey(StudyProgram,
@@ -79,26 +81,26 @@ class Course(models.Model):
     Contains a specific course with a logo for display on the semesterpage.
     Can be connected to several different semesters.
     """
-    full_name = models.CharField('fullt navn',
+    full_name = models.CharField(_('fullt navn'),
                                  unique=True,
                                  max_length=60,
-                                 help_text='F.eks. \"Prosedyre- og Objektorientert Programmering\"'
+                                 help_text=_('F.eks. \"Prosedyre- og Objektorientert Programmering\"')
                                  )
-    display_name = models.CharField('visningsnavn',
+    display_name = models.CharField(_('visningsnavn'),
                                     max_length=60,
-                                    help_text='F.eks. \"C++\"'
+                                    help_text=_('F.eks. \"C++\"')
                                     )
     course_code = models.CharField('emnekode',
                                    primary_key=True,
                                    max_length=10,
-                                   help_text='F.eks. \"TDT4102\"'
+                                   help_text=_('F.eks. \"TDT4102\"')
                                    )
     semesters = models.ManyToManyField(Semester,
                                        related_name='courses'
                                        )
     logo = models.ImageField(upload_to='semesterpage/static/semesterpage/courses')
-    homepage = models.URLField('Fagets hjemmeside',
-                               help_text='F.eks. \"http://home.phys.ntnu.no/fysikkfag/eksamensoppgaver\"'
+    homepage = models.URLField(_('Fagets hjemmeside'),
+                               help_text=_('F.eks. \"http://home.phys.ntnu.no/fysikkfag/eksamensoppgaver\"')
                                )
 
     def __str__(self):
@@ -114,11 +116,11 @@ class LinkCategory(models.Model):
     thumbnail is used on the semester page for styling the list item containig
     the link.
     """
-    name = models.CharField('kategorinavn',
+    name = models.CharField(_('kategorinavn'),
                             primary_key=True,
                             max_length=60
                             )
-    thumbnail = models.ImageField('ikon for kategori',
+    thumbnail = models.ImageField(_('ikon for kategori'),
                                   upload_to='semesterpage/static/semesterpage/link_categories',
                                   blank=True
                                   )
@@ -134,12 +136,12 @@ class Link(models.Model):
     """
     Contains a URL link connected to a specific course.
     """
-    title = models.CharField('tittel',
+    title = models.CharField(_('tittel'),
                              max_length=60,
-                             help_text='F.eks \"Gamle eksamenssett\"'
+                             help_text=_('F.eks \"Gamle eksamenssett\"')
                              )
     url = models.URLField('URL',
-                          help_text='F.eks. \"http://www.phys.ntnu.no/fysikkfag/gamleeksamener.html\"'
+                          help_text=_('F.eks. \"http://www.phys.ntnu.no/fysikkfag/gamleeksamener.html\"')
                           )
     category = models.ForeignKey(LinkCategory)  # e.g. 'Solutions' or 'Plan'
     course = models.ForeignKey(Course,related_name='links')

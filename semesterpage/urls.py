1b116c0269262aca5a5442ed0575f939223e6ff6
Jakob Gerhard Martinussen <jakobgm@gmail.com>
2016-06-21 14:36:42 +0200

    Move navbar template to semesterpage app and use reverse url lookup for the semesterlinks

--- semesterpage/urls.py
+++ semesterpage/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from . import views
 urlpatterns = [
     url(r'^$', views.semester),
-    url(r'^semester/(?P<semester_number>[1-9]|10)/$', views.semester),
-    url(r'^semester/(?P<semester_number>[1-9]|10)/request$', views.user_request),
+    url(r'^semester/(?P<semester_number>[1-9]|10)/$', views.semester, name='semesterpage-semester'),
+    url(r'^semester/(?P<semester_number>[1-9]|10)/request$', views.user_request, name='semesterpage-request'),
 ]

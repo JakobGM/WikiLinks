b42da128d00305e0af55639f1d7a9f0f949cf121
Jakob Gerhard Martinussen <jakobgm@gmail.com>
2016-07-30 04:01:07 +0200

    Customize admin panel with the kokekunster theme

--- kokekunster/settings.py
+++ kokekunster/settings.py
@@ -19,6 +19,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Application definition
 
 INSTALLED_APPS = (
+    'admin_interface',
+    'flat',
+    'colorfield',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- semesterpage/admin.py
+++ semesterpage/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from gettext import gettext as _
 from adminsortable2.admin import SortableInlineAdminMixin
 from rules.contrib.admin import ObjectPermissionsModelAdmin
+from admin_interface.models import Theme
 from .models import StudyProgram, MainProfile, Semester, Course, \
                     ResourceLinkList, CourseLink, ResourceLink, \
                     CustomLinkCategory, Student
@@ -241,3 +242,68 @@ admin.site.register(ResourceLink, ResourceLinkAdmin)
 admin.site.register(CustomLinkCategory, CustomLinkCategoryAdmin)
 admin.site.unregister(User)
 admin.site.register(User, UserAdmin)
+
+
+"""
+Custom styling of the admin site using the admin_interface app.
+Done by editing the default Theme model created by the app at initialization.
+"""
+# Used colors
+white = '#FFFFFF'
+dark_grey = '#555555'
+darker_grey = '#333333'
+grey = '#7A7A7A'
+bright_grey = '#C0C0C0' # silver
+red = '#BA2121'
+dark_red = '#A41515'
+
+# Getting the default theme
+kokekunster_theme = Theme.objects.get_or_create(pk=1)[0]
+
+if kokekunster_theme.name != 'Kokekunster':
+    # Editing the theme
+    kokekunster_theme.name = 'Kokekunster'
+
+    kokekunster_theme.title = _('Kjøkkenet')
+
+    kokekunster_theme.logo = '/static/img/kokekunster_logo.svg'
+
+    kokekunster_theme.css_header_background_color = darker_grey
+    kokekunster_theme.css_header_title_color = white
+    kokekunster_theme.css_header_text_color = bright_grey
+    kokekunster_theme.css_header_link_color = white
+    kokekunster_theme.css_header_link_hover_color = bright_grey
+
+    kokekunster_theme.css_module_background_color = grey
+    kokekunster_theme.css_module_text_color = white
+    kokekunster_theme.css_module_link_color = white
+    kokekunster_theme.css_module_link_hover_color = bright_grey
+    kokekunster_theme.css_module_rounded_corners = False
+
+    kokekunster_theme.css_generic_link_color = dark_grey
+    kokekunster_theme.css_generic_link_hover_color = bright_grey
+
+    kokekunster_theme.css_save_button_background_color = dark_grey
+    kokekunster_theme.css_save_button_background_hover_color = grey
+    kokekunster_theme.css_save_button_text_color = white
+
+    kokekunster_theme.css_delete_button_background_color = red
+    kokekunster_theme.css_delete_button_background_hover_color = dark_red
+    kokekunster_theme.css_delete_button_text_color = white
+
+    kokekunster_theme.css = '#branding h1 span {' \
+                                    'font-size: 30px;' \
+                                    'position: relative; ' \
+                                    'top: 0.5em; ' \
+                                    'font-weight: 300;' \
+                            '}' \
+                            '.login #branding h1 span {' \
+                                    'position: absolute;' \
+                                    'right: 2.8em;' \
+                                    'top: 1.5em;' \
+                            '}' \
+
+
+
+    # Done with the changes, thus writing the changes to the database
+    kokekunster_theme.save()

7b8f5cb62562cabb79d8e99605c4c9f6f8659f65
Jakob Gerhard Martinussen <jakobgm@gmail.com>
2016-07-30 16:14:03 +0200

    Use kjokken as admin URL

--- semesterpage/urls.py
+++ semesterpage/urls.py
@@ -1,8 +1,11 @@
-from django.conf.urls import url
+from django.conf.urls import include, url
+from django.contrib import admin
 from semesterpage import views
 
 urlpatterns = [
     url(r'^$', views.homepage, name='semesterpage-homepage'),
+    url(r'^kjøkken/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
     url(r'^(?P<study_program>[-\w]+)/(?P<main_profile>[-\w]+)/(?P<semester_number>[1-9]|10)/$', views.semester, name='semesterpage-semester'),
     url(r'^(?P<study_program>[-\w]+)/$', views.study_program_view, name='semesterpage-studyprogram'),
     url(r'^(?P<study_program>[-\w]+)/semester/(?P<semester_number>[1-9]|10)/request$', views.user_request, name='semesterpage-request'),

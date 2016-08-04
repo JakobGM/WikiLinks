a7cacd0ddbde75b54273d0d0d4c9d7298c4e94ed
Jakob Gerhard Martinussen <jakobgm@gmail.com>
2016-08-04 18:48:26 +0200

    Remove SemesterAdmin and MainProfileAdmin, and only use inlines in StudyProgramAdmin instead

--- semesterpage/admin.py
+++ semesterpage/admin.py
@@ -9,9 +9,23 @@ from .models import StudyProgram, MainProfile, Semester, Course, \
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import User
 
+class MainProfileInline(admin.TabularInline):
+    """
+    MainProfile Inline in StudyProgramAdmin
+    """
+    model = MainProfile
+    fields = ('full_name', 'display_name',)
+
+class SemesterInline(admin.TabularInline):
+    """
+    Semester Inline in StudyProgramAdmin
+    """
+    model = Semester
+    fields = ('number', 'main_profile', 'published',)
 
 class StudyProgramAdmin(ObjectPermissionsModelAdmin):
     list_display = ('full_name', 'display_name',)
+    inlines = (MainProfileInline, SemesterInline,)
 
     def get_queryset(self, request):
         """
@@ -22,61 +36,6 @@ class StudyProgramAdmin(ObjectPermissionsModelAdmin):
             return super().get_queryset(request)
         else:
             return request.user.contributor.accessible_study_programs()
-
-
-class MainProfileAdmin(ObjectPermissionsModelAdmin):
-    list_filter = ('study_program',)
-
-    def get_queryset(self, request):
-        """
-        Restrict the displayed model instances in the admin view as specifically as possible based on the fields in
-        semesterpage.models.contributor, which is related one-to-one to the User model.
-        """
-        if request.user.is_superuser:
-            return super().get_queryset(request)
-        else:
-            return request.user.contributor.accessible_main_profiles()
-
-    def formfield_for_foreignkey(self, db_field, request, **kwargs):
-        """
-        Restrict the options available for the foreign key field 'study_program' to model instances that the user should
-        have access to. These model instances are found in the semesterpage.models.contributor model, which is related
-        one-to-one to the User model (request.user.contributor).
-        """
-        if not request.user.is_superuser:
-            if db_field.name == 'study_program':
-                kwargs['queryset'] = StudyProgram.objects.filter(pk=request.user.contributor.study_program.pk)
-        return super().formfield_for_foreignkey(db_field, request, **kwargs)
-
-
-class SemesterAdmin(ObjectPermissionsModelAdmin):
-    list_filter = ('study_program',)
-
-    def get_queryset(self, request):
-        """
-        Restrict the displayed model instances in the admin view as specifically as possible based on the fields in
-        semesterpage.models.contributor, which is related one-to-one to the User model.
-        """
-        if request.user.is_superuser:
-            return super().get_queryset(request)
-        else:
-            return request.user.contributor.accessible_semesters()
-
-    def formfield_for_foreignkey(self, db_field, request, **kwargs):
-        """
-        Restrict the options available for the foreign key fields 'study_program' and 'main_profile' to model instances
-        that the user should have access to. These model instances are found in the semesterpage.models.contributor
-        model, which is related one-to-one to the User model (request.user.contributor).
-        """
-        if not request.user.is_superuser:
-            if db_field.name == 'study_program':
-                kwargs['queryset'] = StudyProgram.objects.filter(pk=request.user.contributor.study_program.pk)
-            if db_field.name == 'main_profile':
-                kwargs['queryset'] = request.user.contributor.accessible_main_profiles()
-                if request.user.contributor.main_profile is not None:
-                    # Union of two set
-                    kwargs['queryset'] |= MainProfile.objects.filter(pk=request.user.contributor.main_profile.pk)
-        return super().formfield_for_foreignkey(db_field, request, **kwargs)
 
 
 class CourseLinkInline(SortableInlineAdminMixin, admin.TabularInline):
@@ -208,8 +167,6 @@ class OptionsAdmin(ObjectPermissionsModelAdmin):
 
 
 admin.site.register(StudyProgram, StudyProgramAdmin)
-admin.site.register(MainProfile, MainProfileAdmin)
-admin.site.register(Semester, SemesterAdmin)
 admin.site.register(Course, CourseAdmin)
 admin.site.register(ResourceLinkList, ResourceLinkListAdmin)
 admin.site.register(CustomLinkCategory, CustomLinkCategoryAdmin)
--- semesterpage/rules.py
+++ semesterpage/rules.py
@@ -49,14 +49,10 @@ def create_contributor_groups():
     if created:
         semester_contributors.permissions = []
 
+    # In case of main profile-level specific permissions in the future
     mainprofile_contributors, created = Group.objects.get_or_create(name='mainprofile_contributors')
     if created:
         mainprofile_contributors.permissions = [
-            Permission.objects.get(codename='change_mainprofile'),
-            Permission.objects.get(codename='add_mainprofile'),
-            Permission.objects.get(codename='add_semester'),
-            Permission.objects.get(codename='change_semester'),
-            Permission.objects.get(codename='delete_semester')
         ]
 
     studyprogram_contributors, created = Group.objects.get_or_create(name='studyprogram_contributors')
@@ -65,13 +61,16 @@ def create_contributor_groups():
             Permission.objects.get(codename='change_studyprogram'),
             Permission.objects.get(codename='add_mainprofile'),
             Permission.objects.get(codename='delete_mainprofile'),
-            Permission.objects.get(codename='change_course'),
-            Permission.objects.get(codename='delete_course'),
             Permission.objects.get(codename='add_resourcelinklist'),
             Permission.objects.get(codename='change_resourcelinklist'),
             Permission.objects.get(codename='add_customlinkcategory'),
             Permission.objects.get(codename='delete_resourcelinklist'),
             Permission.objects.get(codename='add_resourcelink'),
             Permission.objects.get(codename='change_resourcelink'),
-            Permission.objects.get(codename='delete_resourcelink')
+            Permission.objects.get(codename='delete_resourcelink'),
+            Permission.objects.get(codename='change_mainprofile'),
+            Permission.objects.get(codename='add_mainprofile'),
+            Permission.objects.get(codename='add_semester'),
+            Permission.objects.get(codename='change_semester'),
+            Permission.objects.get(codename='delete_semester')
         ]

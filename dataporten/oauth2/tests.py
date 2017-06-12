50e7a971e516348dcbf9c6b5cbfc84afc22347d2
Jakob Gerhard Martinussen <jakobgm@gmail.com>
2017-06-12 19:58:06 +0200

    Dataporten login response mock added

--- dataporten/oauth2/tests.py
+++ dataporten/oauth2/tests.py
@@ -17,17 +17,27 @@ class DataportenTest(OAuth2TestsMixin, TestCase):
         Site.objects.create(pk=4, domain='http://127.0.0.1:8000')
 
         super(DataportenTest, self).setUp()
+        self.provider = DataportenProvider
+
+    def get_login_response_json(self, with_refresh_token=True):
+        # Dataporten does not send refresh tokens
+        return '''{
+            "access_token":"testac",
+            "expires_in":3600,
+            "scope": "userid profile groups"
+        }'''
 
     def get_mocked_response(self):
-        return MockedResponse(200, '''
-        {
-        "user": {
-            "userid": "76a7a061-3c55-430d-8ee0-6f82ec42501f",
-            "userid_sec": ["feide:andreas@uninett.no"],
-            "name": "Andreas \u00c5kre Solberg",
-            "email": "andreas.solberg@uninett.no",
-            "profilephoto": "p:a3019954-902f-45a3-b4ee-bca7b48ab507"
-        },
-        "audience": "app123id"
-        }
-        ''')
+        return MockedResponse(
+            200,
+            '''{
+            "user": {
+                "userid": "76a7a061-3c55-430d-8ee0-6f82ec42501f",
+                "userid_sec": ["feide:andreas@uninett.no"],
+                "name": "Andreas \u00c5kre Solberg",
+                "email": "andreas.solberg@uninett.no",
+                "profilephoto": "p:a3019954-902f-45a3-b4ee-bca7b48ab507"
+            },
+            "audience": "app123id"
+            }'''
+            )

e3ccd3aa7429c4de825379880a97d3317cb82011
Jakob Gerhard Martinussen <jakobgm@gmail.com>
2017-08-15 18:33:31 +0200

    Fix bug related to non-superusers using CourseUpload

--- semesterpage/rules.py
+++ semesterpage/rules.py
@@ -11,11 +11,17 @@ def has_contributor_access(user, object):
     return user.is_authenticated \
             and user.contributor.has_contributor_access_to(object)
 
-contributor_models = ['studyprogram', 'mainprofile',
-                      'semester', 'course',
-                      'resourcelinklist', 'customlinkcategory',
-                      'courselink', 'resourcelink',
-                      'options']
+contributor_models = [
+        'course',
+        'courseupload',
+        'customlinkcategory',
+        'mainprofile',
+        'options',
+        'resourcelink',
+        'resourcelinklist',
+        'semester',
+        'studyprogram',
+]
 
 for model in contributor_models:
     add_perm('semesterpage.change_' + model, has_contributor_access)
@@ -23,3 +29,6 @@ for model in contributor_models:
 
 add_perm('semesterpage.change_contributor', is_superuser)
 add_perm('semesterpage.delete_contributor', is_superuser)
+
+# We need to enable users to _add_ new course upload files
+add_perm('semesterpage.add_courseupload', has_contributor_access)

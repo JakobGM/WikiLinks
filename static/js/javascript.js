81fcd26a85904530a7fede5d3332111d1cd50c4d
Jakob Gerhard Martinussen <jakobgm@gmail.com>
2017-08-25 19:44:23 +0200

    Show edit link to all users.
    But use javascript to redirect correctly, or possible inform of denied access.

--- static/js/javascript.js
+++ static/js/javascript.js
@@ -1,5 +1,5 @@
-// Redirects to user-specified 1024-calendar
 function calendarRedirect() {
+  // Redirects to user-specified 1024-calendar
   var calendarName = window.prompt("Tast inn ditt kalendernavn på ntnu.1024.no. " +
                   "Du blir deretter sendt direkte til din kalender neste gang. ");
 
@@ -10,9 +10,9 @@ function calendarRedirect() {
   window.location = "/kalender/" + calendarName;
 }
 
-// Prompts the user for the course's homepage url, redirects to view in order
-// to update the course homepage accordingly
 function courseHomepageRedirect(course_pk) {
+  // Prompts the user for the course's homepage url, redirects to view in order
+  // to update the course homepage accordingly
   var homepageURL = window.prompt("Dette faget har ingen lagret hjemmeside. " +
                   "\n\nVennligst tast inn URLen til fagets hjemmeside.");
 
@@ -32,5 +32,22 @@ function removeCourseFromStudentPage(course_pk) {
   }
   else {
     return;
+  }
+}
+
+function redirectToCourseAdmin(url, authenticated, permission) {
+  // If the user is authenticated and has edit privileges for the given course,
+  // redirect to the Course admin. If the user has not logged in, redirect to
+  // the login provider. Else, inform the user that they do not have edit
+  // privileges for this specific course.
+  if (authenticated && permission) {
+    window.location = url;
+  } else if (!authenticated) {
+   if (window.confirm("Du må være logget inn for å redigere faglenker.\n\n" +
+                      "Du vil nå bli logget inn.")) {
+     window.location = "/accounts/dataporten/login/";
+   }
+  } else {
+    window.alert("Du kan kun redigere fag du er oppmeldt i.");
   }
 }

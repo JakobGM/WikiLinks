aef345463c866009797b3450f3077216c41c9e74
Jakob Gerhard Martinussen <jakobgm@gmail.com>
2018-12-29 19:24:16 +0100

    examiner: Add navbar content in exam archive

--- examiner/views.py
+++ examiner/views.py
@@ -1,9 +1,14 @@
 from typing import Optional
 
+from django.conf import settings
 from django.db.models import F
 from django.shortcuts import render
 
 from examiner.models import PdfUrl
+from semesterpage.models import Course, Semester, StudyProgram
+
+
+DEFAULT_SEMESTER_PK = getattr(settings, 'DEFAULT_SEMESTER_PK', 1)
 
 
 def exams(request, course_code: Optional[str] = None):
@@ -21,12 +26,21 @@ def exams(request, course_code: Optional[str] = None):
             exam__course_code__iexact=course_code.upper(),
         )
 
+    semester_pk = request.session.get('semester_pk', DEFAULT_SEMESTER_PK)
+    try:
+        semester = Semester.objects.get(pk=semester_pk)
+    except Semester.DoesNotExist:
+        semester = None
+
     context = {
         'exam_courses': exam_urls.organize(),
         'user': request.user,
+        'study_programs': StudyProgram.objects.filter(published=True),
+        'semester': semester,
     }
     if course_code:
         context['header_text'] = f' / exams / ' + course_code
+        context['course'] = Course.objects.get(course_code=course_code.upper())
     else:
         context['header_text'] = ' / exams'
 

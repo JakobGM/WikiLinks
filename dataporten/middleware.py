74b0ea2ee50d6cc1117e99c097e510dcb05a8a92
Jakob Gerhard Martinussen <jakobgm@gmail.com>
2019-01-06 17:21:41 +0100

    Remove unused imports

--- dataporten/middleware.py
+++ dataporten/middleware.py
@@ -1,11 +1,7 @@
-import logging
-
-import requests
 import requests_cache
 from django.conf import settings
 from django.http import HttpRequest
 
-from .api import usergroups
 from .models import DataportenUser
 
 # Cache requests for 15 minutes
